a51424d3aca49834ec0392aaa157fc2ec0c9f32e
shas02 <shas02qazxsw@gmail.com>
2021-11-18 18:22:05 +0200

    Saw lesson 1 and made homework 1

---
+++ src/app/components/posts/posts.component.ts
@@ -0,0 +1,24 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Post} from 'src/app/models/Post';
+import {PostService} from 'src/app/services/post.service';
+
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent implements OnInit {
+  posts: Post[] = [];
+
+  constructor(private postService: PostService) {
+  }
+
+  ngOnInit(): void {
+    this.postService.getPosts().subscribe(value => {
+      this.posts = value
+    });
+  }
+
+}
---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {User} from 'src/app/models/User';
+import {UserService} from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+  users: User[] = [];
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe(value => {
+      this.users = value
+    });
+  }
+
+}
